21026c52dce27064f17181cc4518a4297f668a17
tebu0 <estebanarivasv@gmail.com>
2021-10-09 19:39:54 -0300

    Add initial project services

---
+++ rating-service/src/test/java/com/erivas/ratingservice/RatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erivas.ratingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
